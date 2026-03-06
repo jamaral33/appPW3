2e111904818dcaf3d41ead53ecf6bb6130edfab4
jamaral33 <jpgamedev07@gmail.com>
2026-03-06 10:33:14 -0300

    subindo arquivos da aula 1

---
+++ ProjetoPW/ProjetoPW/Models/Usuario.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoPW.Models
+{
+    public class Usuario
+    {
+        [Display(Name = "Código")]
+        public int? IdUsu { get; set; }
+
+        [Display(Name = "Nome Completo")]
+        [Required(ErrorMessage = "O campo nome é obrigatório")]
+        public string nomeUsu { get; set; }
+
+        [Display(Name = "Cargo")]
+        [Required(ErrorMessage = "O campo Cargo é obrigatório")]
+        public string Cargo { get; set; }
+
+        [Display(Name = "Nascimento")]
+        [Required(ErrorMessage = "O campo nascimento é obrigatório")]
+        [DataType(DataType.DateTime)]
+        public DateTime DataNasc { get; set; }
+    }
+
+    }
+
---
+++ ProjetoPW/ProjetoPW/Repository/Contract/IUsuarioRepository.cs
@@ -0,0 +1,17 @@
+﻿using ProjetoPW.Models;
+
+namespace ProjetoPW.Repository.Contract
+{
+    public interface IUsuarioRepository
+    {
+        IEnumerable<Usuario> ObterTodosUsuarios();
+
+        void Cadastrar(Usuario usuario);
+
+        void Atualizar(Usuario usuario);
+
+        Usuario ObterUsuario(int Id);
+
+        void Excluir(int Id);
+    }
+}
