08d78b36b26dfc5a27582ada4da0ac69517bb25b
jamaral33 <jpgamedev07@gmail.com>
2026-03-07 13:34:11 -0300

    Criando o arquivo ususarioRepository

---
+++ ProjetoPW/ProjetoPW/Repository/UsuarioRepository.cs
@@ -0,0 +1,33 @@
+﻿using ProjetoPW.Models;
+
+namespace ProjetoPW.Repository.Contract
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+        private readonly string _ConexaoMySQL; 
+        public void Atualizar(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Cadastrar(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Excluir(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Usuario> ObterTodosUsuarios()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Usuario ObterUsuario(int Id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
